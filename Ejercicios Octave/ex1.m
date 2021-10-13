392303424a6e3549a9e0b4cb6b24b29a42c911dd
Miguel <miguel14lopez99@gmail.com>
2021-10-13 10:33:39 +0200

    Ejemplos en Octave

---
+++ Ejercicios Octave/ex1.m
@@ -0,0 +1,26 @@
+﻿clc
+clear
+M = [ 1 2 3 4 ; 5 6 7 8 ; 9 10 11 12 ; 13 14 15 16 ];
+
+for i=1:1:size(M,1)
+    
+    if rem(i,2) != 0
+        
+        for j=1:1:size(M,2) 
+            
+            fprintf("%i ", M(i,j));
+            
+        end
+        
+    else
+        
+        for j=size(M,2):-1:1
+            
+            fprintf("%i ", M(i,j));
+            
+        end
+        
+    end
+    fprintf("\n")
+    
+end
---
+++ Ejercicios Octave/ex2.m
@@ -0,0 +1,27 @@
+﻿clc
+clear
+
+n = 100;
+
+for i=1:1:n
+
+    for j=1:1:n
+        
+        for k=1:1:n
+            
+            a = i^2 + j^2;
+            b = k^2;
+            
+            if ( a == b && i+j+k == 30 )
+                
+                fprintf("%i^2 + %i^2 = %i^2\n", i, j, k );
+                
+            end
+               
+        end
+        
+    end
+
+end
+
+
