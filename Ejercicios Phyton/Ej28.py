c5cde57c662426117045929024d1783d05cf5b85
Miguel <miguel14lopez99@gmail.com>
2021-11-03 10:49:59 +0100

    ejercicio 28 a medias

---
+++ Ejercicios Phyton/Ej28.py
@@ -0,0 +1,6 @@
+print("the formula to calculate the quadratic equation is:")
+print("")
+
+A = int(input("Ingrese el coeficiente de la variable cuadrática\n"))
+B = int(input("Ingrese el coeficiente de la variable lineal\n"))
+C = int(input("Ingrese el término independiente\n"))
